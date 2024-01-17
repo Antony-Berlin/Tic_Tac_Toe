d608a40369bcb30a462531a710a73614eda38cc6
Antony-Berlin <antonyberlin2003@gmail.com>
2024-01-17 18:59:14 +0530

    added reset game function

--- tic_tac_toe/lib/components/game_tile.dart
+++ tic_tac_toe/lib/components/game_tile.dart
@@ -3,31 +3,15 @@ import 'package:flutter/material.dart';
 class GameTile extends StatelessWidget {
 
   final String gameTileValue;
+  final Function(String?)? onGameTileTapped;
   const GameTile({
     super.key,
-    required this.gameTileValue
+    required this.gameTileValue,
+    required this.onGameTileTapped
   });
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.all(3.0),
-      child: ElevatedButton(
-        
-        style: ButtonStyle(
-          shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(5.0)
-            )
-          ),
-          backgroundColor: MaterialStateProperty.all<Color>(Colors.pink.shade500),
-        ),
-        onPressed: () {}, 
-        child:  Text(
-          gameTileValue, 
-          style: TextStyle(fontFamily: "ArchitectsDaughter-Regular", fontSize: 40.0, fontWeight: FontWeight.bold)
-        )
-      ),
-    );
+    return Placeholder();
   }
 }
--- tic_tac_toe/lib/pages/home_page.dart
+++ tic_tac_toe/lib/pages/home_page.dart
@@ -9,7 +9,70 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  List gameTileValueList = ["X", "O", " ", "X", "O", "O", "X", "X", "X"];
+  List gameTileValueList = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+
+  int player = 1;
+  void onGameTileTapped(index){
+    print(gameTileValueList);
+    
+    if(gameTileValueList[index] == " "){
+      if(player == 1){
+        player = 2;
+        setState(() {
+          gameTileValueList[index] = 'X';
+        });
+      }else{
+        setState(() {
+          player = 1;
+          gameTileValueList[index] = 'O';
+        });
+      }
+    }
+
+    //check if anybody won the game
+    checkWinner();
+  }
+
+  void checkWinner(){
+    int winner = 0;
+    List gTL = gameTileValueList;
+    if (((gTL[4]==gTL[0] && gTL[4]==gTL[8])||
+        (gTL[4]==gTL[1] && gTL[4]==gTL[7])||
+        (gTL[4]==gTL[2] && gTL[4]==gTL[6])||
+        (gTL[4]==gTL[3] && gTL[4]==gTL[5]))&&gTL[4]!=" "
+        )
+    {
+      winner = gTL[4]=="X" ? 1 : 2;
+      print("cond1");
+    }
+    else if(((gTL[0]==gTL[1]&&gTL[0]==gTL[2])||
+            (gTL[0]==gTL[3]&&gTL[0]==gTL[6]))&&gTL[0]!=" "
+    ){
+        winner = gTL[0]=="X" ? 1 : 2;
+        print("cond2");
+    }
+    else if(((gTL[8]==gTL[5]&&gTL[8]==gTL[2])||
+            (gTL[8]==gTL[7]&&gTL[8]==gTL[6]))&&gTL[8]!=" "
+    ){
+        winner = gTL[0]=="X" ? 1 : 2;
+        print("cond3");
+    }
+    if(winner != 0){
+      showDialog(context: context, builder: (context) {
+        return AlertDialog(
+          content: Text("Player $winner won the game"),
+        );
+      });
+      resetGame();
+    }
+  }
+
+  void resetGame(){
+    for(int i = 0; i<gameTileValueList.length; i++){
+      gameTileValueList[i] = " ";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,14 +93,32 @@ class _HomePageState extends State<HomePage> {
                     itemCount: 9,
                     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
                     itemBuilder: (context, index) {
-                      return  GameTile(gameTileValue: gameTileValueList[index]);
+                      return  Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ElevatedButton(
+                          
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.0)
+                              )
+                            ),
+                            backgroundColor: MaterialStateProperty.all<Color>(Colors.pink.shade300),
+                          ),
+                          onPressed: (){onGameTileTapped(index);}, 
+                          child:  Text(
+                            gameTileValueList[index], 
+                            style: TextStyle(fontFamily: "ArchitectsDaughter-Regular", fontSize: 40.0, fontWeight: FontWeight.bold)
+                          )
+                        ),
+                      );
                     }),
               )),
           //play again new game buttons
-          // Expanded(
-          //     child: Container(
-          //   color: Colors.pink,
-          // )),
+          Expanded(
+              child: Container(
+            color: Colors.pink,
+          )),
         ],
       ),
     );
